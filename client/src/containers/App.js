c043776c01105cb6fbd535257526b3ed6a5f7085
Esau Silva <esausilva85@gmail.com>
2017-12-29 10:26:56 -0600

    Replaced react-loadable with react-imported-component

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
-import Loadable from 'react-loadable';
+import ImportedComponent from 'react-imported-component';
 
 import Loading from '../components/Loading';
 
-const AsyncUpdaterForm = Loadable({
-  loader: () => import('../components/UpdaterForm'),
-  loading: Loading
-});
-
-const AsyncLogin = Loadable({
-  loader: () => import('../components/Login'),
-  loading: Loading
-});
-
-const AsyncNoMatch = Loadable({
-  loader: () => import('../components/NoMatch'),
-  loading: Loading
-});
+const AsyncUpdaterForm = ImportedComponent(
+  () =>
+    import(/* webpackChunkName:'UpdaterForm' */ '../components/UpdaterForm'),
+  {
+    LoadingComponent: Loading
+  }
+);
+const AsyncLogin = ImportedComponent(
+  () => import(/* webpackChunkName:'Login' */ '../components/Login'),
+  {
+    LoadingComponent: Loading
+  }
+);
+const AsyncNoMatch = ImportedComponent(
+  () => import(/* webpackChunkName:'NoMatch' */ '../components/NoMatch'),
+  {
+    LoadingComponent: Loading
+  }
+);
 
 const App = () => {
   return (
